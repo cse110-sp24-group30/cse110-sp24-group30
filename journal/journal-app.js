6651d31d6d4ea212f98a27c4112e26fd8c57e575
Kenneth V. <103289124+kennethkietvuong@users.noreply.github.com>
2024-05-16 13:28:50 -0700

    add localStorage functionality to journals

--- journal/journal-app.js
+++ journal/journal-app.js
@@ -1,38 +1,28 @@
 window.addEventListener('DOMContentLoaded', init);
 
-let noteCount = 0;
-
 // Main function that initializes every other function
 function init() {
     const journalContainer = document.getElementById('journal-app');
     const addButton = journalContainer.querySelector('.add-journal');
 
-    // IMPLEMENT:
-    // addButton.addEventListener('click', () => {
-    //     overlay.style.display = 'block';
-    //     modal.style.display = 'block';
-    //     journalContent.value = '';
-    // });
-    addButton.addEventListener('click', () => addJournalNew(journalContainer));
+    addButton.addEventListener('click', () => addJournalNew(journalContainer, false));
 
-    // IMPLEMENT:
+    // When a user clicks "out" of the journal content/popup, it closes it
     overlay.addEventListener('click', () => {
         overlay.style.display = 'none';
         modal.style.display = 'none';
     });
 
-    // addButton.addEventListener('click', () => addJournal(journalContainer))
-
-    // getJournals().forEach(entry => {
-    //     const journalElement = createJournalElement(entry.id, entry.title, entry.documentation, entry.reflection);
-
-    //     journalContainer.append(journalElement);
-    //     noteCount++;
-    // });
+    // Add saved journals from localStorage
+    addJournalNew(journalContainer, true);
 }
 
 // Display the new journal element to the page
-function addJournalNew(journalContainer) {
+function addJournalNew(journalContainer, existing) {
+    
+    const journalList = getJournals();
+    let journalID = 0;
+
     const journalTemplate = {
         id: Math.floor(Math.random() * 2000000),
         title: "",
@@ -42,27 +32,56 @@ function addJournalNew(journalContainer) {
     
     const modalRef = document.getElementById('modal');
 
-    const journalID = createJournalElementNew(journalTemplate.id, journalTemplate.title, 
-        journalTemplate.documentation, journalTemplate.reflection, modalRef);
-    
-    const journalElementTest = document.createElement('div');
-    journalElementTest.classList.add('journal-widget');
-    journalElementTest.textContent = 'Placeholder Title';
-    //journalElementTest.id = journalID;
-    journalElementTest.setAttribute('widget-id', journalID);
+    if (existing) {
+        
+        journalList.forEach(journal => {
+            journalID = createJournalElement(journal.id, journal.title, 
+                journal.documentation, journal.reflection, modalRef);
 
-    journalElementTest.addEventListener('dblclick', function(event) {
-        overlay.style.display = 'block';
-        const currentJournalID = event.target.getAttribute('widget-id');
-        //const currentJournalID = event.target.id; // Get current ID of double clicked journal 
-        const journalEntries = document.querySelectorAll('.journal-entry');
-        hideOtherJournalEntries(journalEntries, currentJournalID);
-        modal.style.display = 'block';
-    });
+                const journalElementTest = document.createElement('div');
+                journalElementTest.classList.add('journal-widget');
+                journalElementTest.textContent = 'Placeholder Title';
+                journalElementTest.setAttribute('widget-id', journalID);
+            
+                journalElementTest.addEventListener('dblclick', function(event) {
+                    overlay.style.display = 'block';
+                    const currentJournalID = event.target.getAttribute('widget-id');
+                    const journalEntries = document.querySelectorAll('.journal-entry');
+                    hideOtherJournalEntries(journalEntries, currentJournalID);
+                    modal.style.display = 'block';
+                });
 
-    journalContainer.append(journalElementTest);
+                journalContainer.append(journalElementTest);
+        });
+    }
+    else {
+        journalID = createJournalElement(journalTemplate.id, journalTemplate.title, 
+            journalTemplate.documentation, journalTemplate.reflection, modalRef);
+            
+            const journalElementTest = document.createElement('div');
+            journalElementTest.classList.add('journal-widget');
+            journalElementTest.textContent = 'Placeholder Title';
+            journalElementTest.setAttribute('widget-id', journalID);
+        
+            journalElementTest.addEventListener('dblclick', function(event) {
+                overlay.style.display = 'block';
+                const currentJournalID = event.target.getAttribute('widget-id');
+                const journalEntries = document.querySelectorAll('.journal-entry');
+                hideOtherJournalEntries(journalEntries, currentJournalID);
+                modal.style.display = 'block';
+            });
+            
+            journalContainer.append(journalElementTest);
+            
+            // Save new journal to localStorage
+            journalList.push(journalTemplate);
+            saveNotes(journalList);
+    }
+
 }
 
+
+// Hides other journals' contents when focusing on a particular journal
 function hideOtherJournalEntries(journalEntries, currentJournalID) {
 
     // Convert nodeList to array & loop through each journal entry
@@ -79,7 +98,7 @@ function hideOtherJournalEntries(journalEntries, currentJournalID) {
 }
 
 // Creates a "sub-div" that will contain each journal's content
-function createJournalElementNew(id, title, documentation, reflection, modalRef) {
+function createJournalElement(id, title, documentation, reflection, modalRef) {
     const journalBody = document.createElement('div'); // sub div per journal
     const journalTitle = document.createElement('textarea');
     const journalDocumentation = document.createElement('textarea');
@@ -100,7 +119,6 @@ function createJournalElementNew(id, title, documentation, reflection, modalRef)
     journalReflection.value = reflection;
     journalReflection.placeholder = 'Insert reflection here';
 
-
     journalBody.append(journalTitle);
     journalBody.append(journalDocumentation);
     journalBody.append(journalReflection);
@@ -110,56 +128,7 @@ function createJournalElementNew(id, title, documentation, reflection, modalRef)
     return journalBody.id;
 }
 
-// Adds new journal entry to page
-// function addJournal(journalContainer) {
-
-//     const entries = getJournals();
-
-//     const entryObject = {
-//         id: Math.floor(Math.random() * 2000000),
-//         title: "",
-//         documentation: "",
-//         reflection: ""
-//     };
-
-//     const journalElement = createJournalElement(entryObject.id, entryObject.title, 
-//                                             entryObject.documentation, entryObject.reflection);
-//     journalContainer.append(journalElement);
-//     entries.push(entryObject);
-//     saveNotes(entries);
-//     noteCount++;
-// }
-
-// Creates new journal entry element 
-function createJournalElement(id, title, documentation, reflection){
-
-    const journalBody = document.createElement('div');
-    const journalTitle = document.createElement('textarea');
-    const journalDocumentation = document.createElement('textarea');
-    const journalReflection = document.createElement('textarea');
-
-    journalBody.classList.add('journal-entry');
-    journalTitle.classList.add('journal-title');
-    journalDocumentation.classList.add('journal-documentation');
-    journalReflection.classList.add('journal-reflection');
-
-    journalTitle.value = title;
-    journalTitle.placeholder = 'Insert title here'
-
-    journalDocumentation.value = documentation;
-    journalDocumentation.placeholder = 'Insert documention here';
-
-    journalReflection.value = reflection;
-    journalReflection.placeholder = 'Insert reflection here';
-
-    journalBody.append(journalTitle);
-    journalBody.append(journalDocumentation);
-    journalBody.append(journalReflection);
-
-    return journalBody;
-}
-
-// Get all the journal entries from local storage
+// Get all the journal entries from localStorage
 function getJournals() {
     return JSON.parse(localStorage.getItem("journal-list") || "[]");
 }
@@ -169,7 +138,7 @@ function getJournal(journals, id) {
     return journals.filter(journals => journals.id == id)[0];
 }
 
-// Saves all journal entries to local storage
+// Saves all journal array to localStorage
 function saveNotes(journals){
     localStorage.setItem("journal-list", JSON.stringify(journals));
 }
