d7421c66fb09ee51c81ee2dcd3cea2f0aefa733b
kabirsachdev7 <kabir.sachdev7@gmail.com>
2024-05-23 14:40:47 -0700

    fix eslint config files for correct testing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,19 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+ // Adjust the file patterns as needed
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+        page: true, // Specific globals for jest-puppeteer
+        browser: true,
+        context: true,
+        jestPuppeteer: true,
+      }
+    }
+  },
   pluginJs.configs.recommended,
 ];
