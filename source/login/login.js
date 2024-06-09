162ed32ae6c5e79d45f2b9483a6b49f1ac416122
Phoebe Tang <49921653+adironene@users.noreply.github.com>
2024-06-09 14:56:44 -0700

    update protected pages

--- source/login/login.js
+++ source/login/login.js
@@ -1,4 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const rootUrl = window.location.pathname.split("/").slice(0, 2).join("/");
+  const protectedPaths = [
+    rootUrl + "/dashboard/",
+    rootUrl + "/dashboard/index.html",
+    rootUrl + "/to_do/",
+    rootUrl + "/to_do/to-do.html",
+    rootUrl + "/journal/",
+    rootUrl + "/journal/journal-page.html",
+    rootUrl + "/calendar/calendar-page.html",
+    rootUrl + "/calendar/",
+  ];
+
+  console.log(protectedPaths);
+  console.log(localStorage.getItem("pinVerified"));
+  const sourcePath = window.location.pathname;
+
+  console.log(sourcePath);
+  if (
+    !sessionStorage.getItem("pinVerified") &&
+    protectedPaths.includes(sourcePath)
+  ) {
+    console.log("got here");
+    window.location.href = rootUrl + "/login/login.html";
+  }
   let correctPin;
   const pinForm = document.getElementById("pin-form");
   const feedbackMessage = document.getElementById("feedback-message");
