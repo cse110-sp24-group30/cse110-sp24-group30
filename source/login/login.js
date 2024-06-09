40f3f0dde124f8ed4dc08865b4835216add1dd1a
kabirsachdev7 <kabir.sachdev7@gmail.com>
2024-06-09 04:28:25 -0700

    Stored pin in config file so its easier to update just incase we want to leave it hardcoded

--- source/login/login.js
+++ source/login/login.js
@@ -1,26 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const correctPin = "1234"; //TODO: need to decide what the pin is
+  let correctPin;
   const pinForm = document.getElementById("pin-form");
 
-  // Check if the user is already authenticated
-  if (sessionStorage.getItem("pinVerified") === "true") {
-    redirectToProtectedPage();
-  }
+  // Fetch the PIN from config.json
+  fetch("config.json")
+    .then((response) => response.json())
+    .then((data) => {
+      correctPin = data.pin;
 
-  // Handle PIN form submission
-  if (pinForm) {
-    pinForm.addEventListener("submit", function (e) {
-      e.preventDefault();
-      const enteredPin = document.getElementById("pin").value;
-      if (enteredPin === correctPin) {
-        sessionStorage.setItem("pinVerified", "true");
+      // Check if the user is already authenticated
+      if (sessionStorage.getItem("pinVerified") === "true") {
         redirectToProtectedPage();
-      } else {
-        alert("Invalid PIN. Please try again.");
       }
-    });
-  }
 
+      // Handle PIN form submission
+      if (pinForm) {
+        pinForm.addEventListener("submit", function (e) {
+          e.preventDefault();
+          const enteredPin = document.getElementById("pin").value;
+          if (enteredPin === correctPin) {
+            sessionStorage.setItem("pinVerified", "true");
+            redirectToProtectedPage();
+          } else {
+            alert("Invalid PIN. Please try again.");
+          }
+        });
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching the PIN:", error);
+    });
   function redirectToProtectedPage() {
     window.location.href = "../dashboard/index.html";
   }
