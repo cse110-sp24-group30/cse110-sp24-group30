3c43d5fc24171e92f331879eec5e78797d73254f
Phoebe Tang <49921653+adironene@users.noreply.github.com>
2024-06-10 00:27:40 -0700

    [login] block link fix

--- source/assets/scripts/main.js
+++ source/assets/scripts/main.js
@@ -3,14 +3,18 @@ window.addEventListener("DOMContentLoaded", init);
 function init() {
   const rootUrl = window.location.pathname.split("/").slice(0, 2).join("/");
   const protectedPaths = [
-    rootUrl + "/dashboard/",
-    rootUrl + "/dashboard/index.html",
+    rootUrl + "/",
+    rootUrl + "/index.html",
     rootUrl + "/to_do/",
     rootUrl + "/to_do/to-do.html",
     rootUrl + "/journal/",
     rootUrl + "/journal/journal-page.html",
     rootUrl + "/calendar/calendar-page.html",
     rootUrl + "/calendar/",
+    "https://cse110-sp24-group30.github.io/cse110-sp24-group30/source/calendar/calendar-page.html",
+    "https://cse110-sp24-group30.github.io/cse110-sp24-group30/source/to_do/to-do.html",
+    "https://cse110-sp24-group30.github.io/cse110-sp24-group30/source/journal/journal-page.html",
+    "https://cse110-sp24-group30.github.io/cse110-sp24-group30/source/index.html"
   ];
 
   console.log(protectedPaths);
